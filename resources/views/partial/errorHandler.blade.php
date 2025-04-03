c7b7fc8fb56f9657d83bd6fb1bd3be2ef0211547
Mahdi732 <ey449751@gmail.com>
2025-04-03 19:53:09 +0000

    creat epage erro handler and affiche it dynamicly

--- resources/views/partial/errorHandler.blade.php
+++ resources/views/partial/errorHandler.blade.php
@@ -1,15 +1,17 @@
-<div id="success-notification">
-    <div class="mt-2 bg-green-50 border-l-4 border-green-500 text-green-700 p-3 rounded-r-lg">
+<div id="error-notification">
+    <div class="mt-2 bg-red-50 border-l-4 border-red-500 text-red-700 p-3 rounded-r-lg">
         <div class="flex items-center">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-green-600 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-red-600 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
             </svg>
-            <span class="text-green-800 font-medium">Your profile has been successfully updated!</span>
+            @if(isset($error))
+            <span class="text-red-800 font-medium">{{ $error }}</span>
+            @endif
         </div>
     </div>
 </div>
 <script>
     setTimeout(() => {
-      document.getElementById('success-notification').innerHTML = '';
+      document.getElementById('error-notification').innerHTML = '';
     }, 5000);
 </script>
