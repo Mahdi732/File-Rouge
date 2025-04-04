efdef024a44deb843657cb1dbbcdccb0fa105b07
Mahdi732 <ey449751@gmail.com>
2025-04-04 06:07:25 +0000

    add some relation in friend model

--- app/Models/friends.php
+++ app/Models/friends.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class friends extends Model
 {
     use HasFactory;
+    
+    protected $fillable = [
+        'sender_id',
+        'receiver_id',
+        'status',
+    ];
+
 }
