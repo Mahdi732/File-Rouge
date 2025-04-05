0abf9db841cfd6268743a604b0444f7405453200
Mahdi732 <ey449751@gmail.com>
2025-04-05 22:14:24 +0000

    create new method and implement it

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,8 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 
 class PostController extends Controller
 {
-    //
+    public function index () {
+        $Authuser = Auth::user();
+        $posts = DB::table('posts')
+        ->where('user_id', $Authuser)
+        ->select('posts.*')
+        ->get();
+
+        return view('media', [
+            'posts' => $posts,
+        ]);
+    }
 }
