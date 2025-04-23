958723e2a201616b72878a959cdc63bdf30fc57b
Mahdi732 <ey449751@gmail.com>
2025-04-23 15:56:45 +0100

    feat: update media view with post creation and management interface

--- resources/views/media.blade.php
+++ resources/views/media.blade.php
@@ -157,7 +157,7 @@
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 inline mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
                             </svg>
-                            Delete Media
+                            Delete Post
                         </button>
                     </form>        
 
@@ -167,14 +167,6 @@
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
                         </svg>
                         Edit Post
-                    </a>
-                    
-                    <!-- Delete Option -->
-                    <a href="#" @click="showMenu = false" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 inline mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-                        </svg>
-                        Delete Post
                     </a>
                     
                     <!-- Private/Public Toggle -->
@@ -324,8 +316,9 @@
                 </button>
             </div>
             
-            <div class="mb-1">
-                <span class="font-semibold">{{ '@' . $post->user_name }}</span> {{ $post->description ?? 'hahhaha' }}
+            <div class="mb-1 break-words  overflow-hidden">
+                <span class="font-semibold">{{ '@' . $post->user_name }}</span>
+                {{ $post->description ?? 'hahhaha' }}
             </div>
             <div class="text-xs text-gray-500 mb-2">{{ \Carbon\Carbon::parse($post->created_at)->diffForHumans() }}</div>
             
