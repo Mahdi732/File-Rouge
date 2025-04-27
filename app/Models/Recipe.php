4c35b84915034ed71d3725887be3880ba3ae2ca3
Mahdi732 <ey449751@gmail.com>
2025-04-27 18:24:11 +0100

    Update Recipe model

--- app/Models/Recipe.php
+++ app/Models/Recipe.php
@@ -8,4 +8,26 @@ use Illuminate\Database\Eloquent\Model;
 class Recipe extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'userId',
+        'title',
+        'etap',
+        'description',
+        'note',
+        'video',
+        'image',
+        'ingredients',
+        'timepreparation',
+        'levelPreparation'
+    ];
+
+    protected $casts = [
+        'etap' => 'array', 
+        'ingredients' => 'array',
+    ];
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class, 'category_recipe');
+    }    
 }
